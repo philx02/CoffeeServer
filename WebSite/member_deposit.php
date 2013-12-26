8324998e7ddedd760808bcbb7e068f7b1f56ef69
Philippe Cayouette <Philippe Cayouette@localhost>
2013-12-26 11:12:10 -0500

    wip

--- WebSite/member_deposit.php
+++ WebSite/member_deposit.php
@@ -2,11 +2,35 @@
 include("header.php");
 
 $dbHandle = new SQLite3('test.db', SQLITE3_OPEN_READWRITE);
-$stmt = "SELECT id, name FROM members";
+
+if (array_key_exists("ac", $_POST))
+{
+  if ($_POST["ac"] == "member_deposit")
+  {
+    $amount_cents = floor($_POST["amount"] * 100);
+    $stmt = "UPDATE members SET balance_cents = balance_cents + ".$amount_cents." WHERE userid = ".$_POST["memberid"];
+    echo $stmt."<br/>";
+  }
+  else
+  {
+    echo "<p>Wrong post action.</p>";
+  }
+}
 ?>
 
-<select name="member">
-<?php 
+<form action="member_deposit.php" method="post">
+<input type="hidden" name="ac" value="member_deposit">
+<table>
+<tr>
+<td>Name</td>
+<td>Amount</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>
+<select name="memberid">
+<?php
+$stmt = "SELECT id, name FROM members";
 $result = $dbHandle->query($stmt);
 while ($row = $result->fetchArray())
 {
@@ -14,3 +38,9 @@ while ($row = $result->fetchArray())
 }
 ?>
 </select>
+</td>
+<td><input type="text" name="amount"/></td>
+<td><input type="submit" value="Submit"></td>
+</tr>
+</table>
+</form>
