151786fc7ab15a0db2b83c8e48bbd69d27874f03
Philippe Cayouette <Philippe Cayouette@localhost>
2013-12-28 15:56:30 -0500

    fixed login issues

--- WebSite/main.php
+++ WebSite/main.php
@@ -18,6 +18,7 @@ echo "Current balance: ".number_format((float)($result/100), 2)."$";
 <td width=100 valign="top">
 <p><a href="transaction_history.php?<?php echo "id=".$_SESSION["id_logged"]; ?>" target="inlineframe">Transaction History</a></p>
 <p><a href="member_deposit.php" target="inlineframe">Member Deposit</a></p>
+<p><a href="logout.php">Logout</a></p>
 </td>
 <td>
 <iframe name="inlineframe" src="transaction_history.php?<?php echo "id=".$_SESSION["id_logged"]; ?>" frameborder="0" scrolling="auto" width=1100 height=1000 marginwidth=5 marginheight=5></iframe> 
