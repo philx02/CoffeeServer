c8610147e83ec4c1430c6af95c0bb20d8c32d5b4
Philippe Cayouette <26016182+philx02@users.noreply.github.com>
2013-12-28 14:05:28 -0500

    resolved _SESSION problem

--- WebSite/check_logged.php
+++ WebSite/check_logged.php
@@ -2,7 +2,7 @@
 function check_logged()
 { 
   global $_SESSION;
-  if (!array_key_exists("logged", $_SESSION))
+  if (!array_key_exists("id_logged", $_SESSION))
   {
     header("Location: index.php");
   }
