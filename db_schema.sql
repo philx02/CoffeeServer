f01d65af6d82c8093c2580d1492a81794b635b84
Philippe Cayouette <Philippe Cayouette@localhost>
2014-01-02 22:35:16 -0500

    new schema with constraints

--- db_schema.sql
+++ db_schema.sql
@@ -1,7 +1,44 @@
 BEGIN TRANSACTION;
-CREATE TABLE members (id INTEGER PRIMARY KEY, userid TEXT, username TEXT, password TEXT, name TEXT, email TEXT, balance_cents INTEGER, admin INTEGER);
-INSERT INTO members VALUES(1,0123456789,'cayouette','9a73ee8196e7bde96471721da72a708d10e86c0d','Philippe Cayouette','philippe.cayouette@cae.com',0,1);
-CREATE TABLE numeric_parameters (id INTEGER PRIMARY KEY, name TEXT, value NUMERIC);
+
+CREATE TABLE members
+(
+  id INTEGER PRIMARY KEY,
+  userid TEXT UNIQUE NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  balance_cents INTEGER NOT NULL DEFAULT 0,
+  admin INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE numeric_parameters
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  value NUMERIC NOT NULL
+);
+
+CREATE TABLE transactions
+(
+  id INTEGER PRIMARY KEY,
+  date_time TEXT NOT NULL,
+  member_id INTEGER NOT NULL,
+  transaction_type INTEGER NOT NULL,
+  amount_cents INTEGER NOT NULL,
+  FOREIGN KEY (member_id) REFERENCES members (id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions_name
+(
+  id INTEGER PRIMARY KEY,
+  transaction_type INTEGER UNIQUE NOT NULL,
+  name TEXT NOT NULL
+);
+
+INSERT INTO members VALUES(1,123456789,'cayouette','9a73ee8196e7bde96471721da72a708d10e86c0d','Philippe Cayouette','philippe.cayouette@cae.com',0,1);
+INSERT INTO transactions_name VALUES (1,0,'Deposit');
+INSERT INTO transactions_name VALUES (2,1,'Coffee purchase');
 INSERT INTO numeric_parameters VALUES(1,'unit_cost_cents',10);
-CREATE TABLE transactions (id INTEGER PRIMARY KEY, date_time TEXT, member_id INTEGER, transaction_type INTEGER, amount_cents INTEGER);
+
 COMMIT;
